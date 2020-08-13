e5f16ae3371bf699c18047e6b677ec9751744a8b
Agustin Avendaño <agushaven@gmail.com>
2020-08-13 14:05:10 -0300

    Docs Agregado

---
+++ docs/precache-manifest.de9bfbc28aa923b4d940f756299c1ebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d28f9793b2d34317790312b48042f05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c6d93f53de0824f887e",
+    "url": "/static/css/main.456b4da3.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c6d93f53de0824f887e",
+    "url": "/static/js/main.01768eb2.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
